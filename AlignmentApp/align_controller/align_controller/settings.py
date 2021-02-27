ab307a4de542cbe79364d7e36038f3151f805f38
Ramya Dhani Srinatha <ds.ramya@gmail.com>
2021-02-27 16:57:18 -0500

    Update settings.py

--- AlignmentApp/align_controller/align_controller/settings.py
+++ AlignmentApp/align_controller/align_controller/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e-6c#p^gwd*_1dkhi-=6unc8#)$tq5-)bzi!kn75qymb69ab5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
